4f4417f0a574ece5a700b342515fdb8139c5bf8e
Sultan Kanaan <98963963+sultan-kanaan@users.noreply.github.com>
2022-04-05 14:19:59 +0300

    Add project files.

---
+++ Sultan-Lending-Library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sultan_Lending_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
